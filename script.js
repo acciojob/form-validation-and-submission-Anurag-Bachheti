e1193a0e9fcfe5c3b3ea937bd2ebb48fb6bdb00e
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-08-29 13:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+const checkbox = document.getElementById("terms");
+const submitBtn = document.getElementById("submitBtn");
+const form = document.getElementById("myForm");
+
+checkbox.addEventListener("click", function(){
+	submitBtn.disabled = !this.checked
+})
+
+form.addEventListener("click", function(event){
+	if(!form.checkValidity()){
+		event.preventDefault();
+		alert("Please complete all fields before submitting.");
+	}
+})
